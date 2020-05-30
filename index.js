8f1288cef5ba56427b1c18e58531a2c23d15f684
twong540 <twong@reliaslearning.com>
2020-05-30 16:11:05 -0400

    ESLint added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 8080
-const ip = require('ip');
+/*jslint es6 */
+import express from 'express';
+import { address } from 'ip';
+const app = express();
+const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!<br><br>Local IP =' + ip.address()))
+app.get('/', (req, res) => res.send('Hello World!<br><br>Local IP =' + address()));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
