68706b025a54a8affe767d2b9472c1997cec7541
Tim Wong <timothy.t.wong@gmail.com>
2020-06-13 19:34:39 -0400

    Making a change to trigger rolling update for my app

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const ip = require('ip');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is my Capstone NodeJS App!<br/><br/>Local IP =' + ip.address()));
+app.get('/', (req, res) => res.send('<img src="https://media1.tenor.com/images/b0405fa5f1a494690eb738efd549d432/tenor.gif?itemid=15805264"/><br><br>This is my Capstone NodeJS App!<br/><br/>Local IP =' + ip.address()));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
